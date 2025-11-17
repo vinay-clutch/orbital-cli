d5ce0bfa44762f192a3b6683648202462fc6d471
Vinay B S <vinayvinay0256@gmail.com>
2025-11-17 17:48:30 +0530

    Initial clean commit: add client and server, apply root .gitignore (untrack node_modules)

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-amber-100">
+       <Button> click me </Button>
+    </div>
+
+  );
+}
